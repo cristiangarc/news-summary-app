28cd81cd7dc9fd0f120beb84c417b7471bb7d042
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-08 13:48:12 -0400

    set the articles state using the returned data from the API fetch call to the NYT RSS feed

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,14 +1,22 @@
-import React, {useState} from "react";
-import axios from "axios"
+import { useState, useEffect } from "react";
 import { getNYTArticles } from "./components/fetch";
 
-
 function Homepage () {
-    getNYTArticles('HomePage')
+    const [articles, setArticles] = useState([]);
+    
+    useEffect(() => {
+        getNYTArticles('HomePage').then((arts) => {
+            setArticles(arts);
+        }).catch((err) => console.error(err));
+    }, []);
 
     return (
         <div>
-            
+            {console.log(articles)}
+            <h1>HomePage</h1>
+            {articles.length > 0 && articles.map(article => <li>
+                {article.title}
+            </li>)}
         </div>
     )
 }
