f9ce689ea6fdc38e43523ed56c0578b6f0d47b48
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-08 13:57:57 -0400

    added a link to each article and a description of each article

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import { getNYTArticles } from "./components/fetch";
+import { Link } from "react-router-dom";
 
 function Homepage () {
     const [articles, setArticles] = useState([]);
-    
+
     useEffect(() => {
         getNYTArticles('HomePage').then((arts) => {
             setArticles(arts);
@@ -14,8 +15,9 @@ function Homepage () {
         <div>
             {console.log(articles)}
             <h1>HomePage</h1>
-            {articles.length > 0 && articles.map(article => <li>
-                {article.title}
+            {articles.length > 0 && articles.map(article => <li key={article.guid}>
+                <Link to={article.link} target="_blank">{article.title}</Link>
+                <p>{article.description}</p>
             </li>)}
         </div>
     )
