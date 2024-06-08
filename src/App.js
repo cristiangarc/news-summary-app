e720c705b951f02fbb76103f6a833e0a4cc1a27e
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-08 16:33:12 -0400

    add a route for a particular section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Homepage from './Homepage';
+import Section from './components/Section';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Homepage/>} />
+      <Route path='/sections/:id' element={<Section/>} />
     </Routes>
   );
 }
