f99a26392a6623c8e1c36ef337314b591e079b39
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-08 13:49:07 -0400

    get the array of articles from the parsed XML response

--- src/components/fetch.js
+++ src/components/fetch.js
@@ -1,10 +1,9 @@
 // const API_KEY = import.meta.env.VITE_API_KEY;
-
 import { XMLParser } from "fast-xml-parser";
 
-export function getNYTArticles (section) {
+export async function getNYTArticles (section) {
     return fetch(`https://rss.nytimes.com/services/xml/rss/nyt/${section}.xml`)
         .then(response => response.text())
         .then(text => new XMLParser().parse(text))
-       .then(data => console.log(data.rss.channel));
+       .then(data => data.rss.channel.item);
 }
