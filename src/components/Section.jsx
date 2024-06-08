05ea09c75a54a483bec1c8fb7a3ce20e2bdac8b6
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-08 16:32:39 -0400

    display the articles for the given section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,15 +1,29 @@
-import { Link } from "react-router-dom"
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom"
+import { getNYTArticles } from "./fetch";
+const sectionsJSON = require('../data/NYT-sections.json');
 
-const Section = ({ articles }) => {
-  return (
-    <div>
-        {articles.length > 0 && articles.map(article =>
-            <li key={article.guid}>
-                <Link to={article.link} target="_blank">{article.title}</Link>
-                <p>{article.description}</p>
-            </li>)}
-    </div>
-  )
+const Section = () => {
+    const [articles, setArticles] = useState([]);
+    const { id } = useParams();
+    const sectionsArr = Array.from(sectionsJSON);
+
+    useEffect(() => {
+        getNYTArticles(sectionsArr[id]).then((arts) => {
+            setArticles(arts);
+        }).catch((err) => console.error(err));
+    }, []);
+
+    return (
+        <div>
+            {articles.length > 0 && articles.map(article =>
+                <li key={article.guid}>
+                    <Link to={article.link} target="_blank">{article.title}</Link>
+                    <p>{article.description}</p>
+                    <p>{article['media:description']}</p>
+                </li>)}
+        </div>
+    )
 }
 
 export default Section  
