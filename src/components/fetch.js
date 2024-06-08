030bfff78ed99c7766eda69242260ca19965191e
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-08 12:03:34 -0400

    created a function that fetches NYT articles through RSS feeds

---
+++ src/components/fetch.js
@@ -0,0 +1,7 @@
+// const API_KEY = import.meta.env.VITE_API_KEY;
+
+export function getNYTArticles (section) {
+    return fetch(`https://rss.nytimes.com/services/xml/rss/nyt/${section}.xml`)
+       .then(response => response.json())
+       .then(data => console.log(data));
+}
