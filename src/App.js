cd60442d88664b40a65b6c859fee7acea3f5cbb3
Cristian Garcia <crisg@sas.upenn.edu>
2024-06-08 13:00:14 -0400

    updated the app to use BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import Homepage from './Homepage';
 
 function App() {
